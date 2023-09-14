b57782eb2cf00c3f55146dd1ab560e0e79d23d89
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-09-14 14:40:11 +0400

    feat: add route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { connection } from "./config/mongo.js";
-
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import getPilot from "./controllers/pilotController.js";
 const app = express();
 
 dotenv.config();
 connection();
 
+app.use(bodyParser.json());
+app.get("/pilot/:name", getPilot);
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is listening on port 3000");
 });
