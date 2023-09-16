50ad5856e31cb55443292d21f340599287115d26
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-09-16 21:47:14 +0400

    feat: add solutions for visibility tour

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { connection } from "./config/mongo.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import getPilot from "./controllers/pilotController.js";
+import getVis from "./controllers/visibilityController.js";
 const app = express();
 
 dotenv.config();
@@ -10,6 +11,8 @@ connection();
 
 app.use(bodyParser.json());
 app.get("/pilot/:name", getPilot);
+app.get("/visibility/:name", getVis);
+
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is listening on port 3000");
 });
