c23ed183c73aa6eee2b9105f405ccc66377229a2
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-09-17 18:01:19 +0400

    feat: add route for cursor

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import getPilot from "./controllers/pilotController.js";
 import getVis from "./controllers/visibilityController.js";
+import getCursor from "./controllers/cursorController.js";
 const app = express();
 
 dotenv.config();
@@ -12,6 +13,7 @@ connection();
 app.use(bodyParser.json());
 app.get("/pilot/:name", getPilot);
 app.get("/visibility/:name", getVis);
+app.get("/cursor/:name", getCursor);
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is listening on port 3000");
