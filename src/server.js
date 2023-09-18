ef0d3ba2784ae3d68965dfb65cda3741a301c382
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-09-18 21:02:05 +0400

    feat: add route for display

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import getPilot from "./controllers/pilotController.js";
 import getVis from "./controllers/visibilityController.js";
 import getCursor from "./controllers/cursorController.js";
+import getDisplay from "./controllers/displayController.js";
 const app = express();
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.get("/pilot/:name", getPilot);
 app.get("/visibility/:name", getVis);
 app.get("/cursor/:name", getCursor);
+app.get("/display/:name", getDisplay);
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is listening on port 3000");
